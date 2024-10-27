a8b91d77e397586b74c19d5e3ade2df465cc431c
Tony Nguyen <tnguyen411@gmail.com>
2024-10-27 11:34:03 -0700

    setup packages and express app

---
+++ index.js
@@ -0,0 +1,17 @@
+//1. Import packages - express/axios/ejs/bodyparser
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+//2. Crearte express app and set port number
+const app = express();
+const port = 3000;
+
+//3. Use public folder for static files
+
+//4. Render homepage index.ejs
+
+//5. Pull 4 recipes from the Meal API and push to index.ejs
+
+//6. Listen on port 3000 on server start
+
